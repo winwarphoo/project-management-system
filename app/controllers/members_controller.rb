c6706b57a1b76397a2c7227b283bbbbcc21b899e
Win War Phoo <winwarphoo@mm.cybermissions.co.jp>
2021-10-22 05:17:01 +0000

    メンバーの追加

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,7 +1,7 @@
 class MembersController < ApplicationController
 
   def index
-    @members = Member.all
+    @team = Team.find(params[:team_id])
   end
 
   def create
@@ -9,12 +9,19 @@ class MembersController < ApplicationController
     @member.team_id = params[:team_id]
     if @member.save
       flash[:notice] = "メンバーを正常に追加しました。"
-      redirect_to("/teams/#{params[:team_id]}")
+      redirect_to("/teams/#{params[:team_id]}/members")
     else
       @team = Team.find(params[:team_id])
       flash.now[:alert] = "失敗！"
       render "teams/show"
     end
+  end
+
+  def destroy
+    @member = Member.find(params[:id])
+    @member.destroy
+    flash[:notice] = "メンバーの削除は正常に完了しました。"
+    redirect_to("/teams")
   end
 
   private
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,5 @@
 class Member < ApplicationRecord
   belongs_to :team
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 30 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
